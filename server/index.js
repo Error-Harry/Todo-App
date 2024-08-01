f6325f3d81bbfe89ec5e6e96ffe9c8e1215867d1
Harsh Nargide <nargideharsh@gmail.com>
2024-08-01 23:55:21 +0530

    Initial setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/todo", (req, res) => {});
+
+app.get("/todos", (req, res) => {});
+
+app.get("/completed", (req, res) => {});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
